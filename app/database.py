897a6d8bc69bfd21290dbe809928256707638409
AsakiIchiwa <asakiichiwa@gmail.com>
2026-03-06 22:29:14 +0700

    Update database connection to require SSL
    Added SSL requirement to database connection arguments.

--- app/database.py
+++ app/database.py
@@ -7,7 +7,8 @@ engine = create_async_engine(
     echo=settings.DEBUG,
     pool_pre_ping=True,
     pool_size=5,
-    max_overflow=10
+    max_overflow=10,
+    connect_args={"ssl": "require"}
 )
 
 AsyncSessionLocal = async_sessionmaker(
